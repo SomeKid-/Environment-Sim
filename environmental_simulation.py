c133242330e5370b8ab0d7baa78d1979c476c710
SomeKid- <nshickman@gmail.com>
2014-11-10 16:48:00 -0800

    fixed loop isue

--- environmental_simulation.py
+++ environmental_simulation.py
@@ -4,43 +4,43 @@
 
 import random 
 import time 
+import varb
 
+'''Everything below this point and above the next point has been moved to varb'''
 '''run_number is the variable that tells the code how many times it is allowed to reset the life count. This number will change when the program goes to alpha.'''
-run_number = 5
+#run_number = 5
 
-life = False
+#life = False
 
 '''total number of lives in the sim'''
-life_count = 0 
+#life_count = 0 
 
 '''Numbers of specific forms of life'''
-plant_count = 0
-BlueCow_count = 0
-Fang_count = 0
+#plant_count = 0
+#BlueCow_count = 0
+#Fang_count = 0
+'''Everything above this point has been moved to varb'''
 
-
-
-def creature():
-	'''This will eventually be replaced by various creature functions '''
+'''def creature():
 	print("normal creature functioning goes here")
 	life=True
-	return life
+	life_count=life_count+1
+	return life'''
 
-def spawn_new(type):
-	if type == "BlueCow":
+def spawn_new(life_form):
+	if life_form == "BlueCow":
 		print("BlueCow is added now")
-	elif type == "Fang":
+	elif life_form == "Fang":
 		print("Fang is added here")
-	elif type == "plant":
+	elif life_form == "plant":
 		print("plant is added here")
-	return type
+	return life_form
 	
 def life_check():
-	for a in range(0,run_number):
-		if life == True:
-			life_count = life_count + 1
-			print(life_count)
-		else:
-			spawn_new("BlueCow")
-
+	if varb.life == True:
+		print(varb.life_count)
+		print(varb.life)
+	else:
+		print(varb.life_count)
+		print(varb.life)		
 			
